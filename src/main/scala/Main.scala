5942fdd3ac3047a30c24e850255f985d0d9481f3
Matthias Berndt <matthias_berndt@gmx.de>
2021-08-23 00:54:46 +0200

    simplify

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@ import java.nio.file.*
 import scodec.bits.BitVector
 import scodec.Attempt
 import scodec.DecodeResult
-import java.nio.charset.StandardCharsets
+import java.nio.charset.StandardCharsets.UTF_8
 @main def main(in: String, out: String) =
   val path = Paths.get(in)
   val bytes = Files.readAllBytes(path)
@@ -14,7 +14,7 @@ import java.nio.charset.StandardCharsets
           s"$c1,$c2,$c3,$c4,$d1,$d2,$d3,$d4\n"
         }.mkString + "\n"
       }.mkString
-      Files.writeString(Paths.get(out), text, StandardCharsets.UTF_8)
+      Files.writeString(Paths.get(out), text, UTF_8)
     case Attempt.Failure(err) =>
       println(err)
       sys.exit(1)
